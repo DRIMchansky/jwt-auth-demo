3bd80c149eea44d395593b42325d0152df7963a4
Nikita Chernov <drimchansky@gmail.com>
2023-07-09 12:39:30 +0400

    client: update vite config server proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import 'dotenv/config'
+
 export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': 'http://localhost:4400'
+        '/api': process.env.API_URI
       }
     }
   }
